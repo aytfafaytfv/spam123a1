8164ea90e712497df33ae4a98ee6a1bceb9948e0
aytfafaytfv <56179890+aytfafaytfv@users.noreply.github.com>
2019-11-10 19:29:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("NDA5MDk1OTA3NTgyMjc5Njgw.XchH1A.-EyI8_bqlcQDFXBW_AE3IyA-QgY")
+let channel =     client.channels.get("642799355539095563")
 setInterval(function() {
 channel.send(`Spam by Fareseslam Or Evil ARE back`);
 }, 30)
